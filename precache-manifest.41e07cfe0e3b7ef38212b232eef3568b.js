83ec7d0e1c0f5f9c1141127bb722dabebed31372
Muhammad Rajput <muhammadrajput99@gmail.com>
2020-10-20 18:52:07 -0400

    Updates

---
+++ precache-manifest.41e07cfe0e3b7ef38212b232eef3568b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c2cb2ec0a710bada48fa704dc334e0",
+    "url": "/3688website/index.html"
+  },
+  {
+    "revision": "8b4718c1e417445ec9c9",
+    "url": "/3688website/static/css/main.19cfe48f.chunk.css"
+  },
+  {
+    "revision": "a92fbbbeba9a9d8f38b4",
+    "url": "/3688website/static/js/2.22ea89c0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/3688website/static/js/2.22ea89c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4718c1e417445ec9c9",
+    "url": "/3688website/static/js/main.62684c37.chunk.js"
+  },
+  {
+    "revision": "34f0fbce403fc437960d",
+    "url": "/3688website/static/js/runtime-main.35595e96.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/3688website/static/media/Montserrat-Light.409c7f79.ttf"
+  }
+]);
